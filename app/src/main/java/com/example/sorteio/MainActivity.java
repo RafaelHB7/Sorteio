5c8202b041b59c8977af266b9c8e6551fb8c2e5e
RafaelHB7 <rafael.hobi9@gmail.com>
2020-02-18 21:12:20 -0300

    Funções adicionadas

--- app/src/main/java/com/example/sorteio/MainActivity.java
+++ app/src/main/java/com/example/sorteio/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
+    EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv=findViewById(R.id.textView);
+        et.findViewById(R.id.editText);
+    }
+
+    public void copiaTexto(View view){
+        tv.setText(et.getText().toString());
     }
 }
